6f4ff732d2a7030f990d820cb4201a82e3925d56
crybaby-afk <toolarry55@gmail.com>
2025-01-06 16:57:15 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+    # pass
 
 def greet(name):
-    pass
+    print("Hello, " + name+"!")
+greet("larry")
 
 def greet_with_default(name="programmer"):
-    pass
+    print("Hello, " + name +"!") 
+greet_with_default("Larry")
 
 def add(num1, num2):
-    pass
+    return(num1 + num2)
+print(add(2,4))
+    # pass
 
 def halve(number):
-    pass
+    return(number/2)
+print(halve(88))
+    
